67ca3458df9db0d2b36f870aaa36b14ac77353a0
Lautaro Gonzalez <59624470+Lautaro124@users.noreply.github.com>
2021-11-11 15:51:21 -0300

    Styles

--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-scroll'
-import styles from '../../styles/default.module.css'
+import defaults from '../../styles/default.module.css'
+import styles from '../../styles/landing.module.css'
 
 export default function LandingPage() {
     return (
-        <section id="landing-page" className={styles.titleContent}>
-            <h1>Bienvenido a mi portafolio</h1>
-            <h4>Me llamo lautaro Gonzalez y soy un desarrollador full stack</h4>
+        <section id="landing-page" className={styles.content}>
+            <h1 className={styles.title} >Bienvenido a mi portafolio</h1>
+            <h3 className={styles.subtitle} >Me llamo lautaro Gonzalez y soy desarrollador full stack</h3>
             <Link to='page1' spy={true} smooth={true} delay={80}>
-                <button variant='contained' className={styles.links}>
+                <button variant='contained' className={defaults.links}>
                     About me
                 </button>
             </Link>
